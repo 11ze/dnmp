38d580a1f6fb910f25ad3100c5f93eb4f2d0b6db
11ze <wangzecn@qq.com>
2023-11-20 23:19:29 +0800

    feat: 添加 crontabs 和 Redis 配置

--- services/phpmyadmin/config.user.inc.php
+++ services/phpmyadmin/config.user.inc.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @example https://docs.phpmyadmin.net/en/latest/config.html
  */
@@ -17,3 +18,6 @@ $cfg['Servers'][$i]['verbose']   = 'no2';
 $cfg['Servers'][$i]['host']      = 'mysql5';
 */
 
+$cfg['LoginCookieValidity'] = 1440000; // 登录有效期，秒
+$cfg['MaxNavigationItems'] = 100; // 导航树每页的表数量
+$cfg['MaxRows'] = 100; // 每页行数
